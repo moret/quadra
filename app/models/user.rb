c1f4dc64b800ee0bf017447be3ccab5627caa7c2
Danilo Moret <github@moret.pro.br>
2014-05-12 10:56:11 -0300

    removed forgotten method

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,28 +13,3 @@ class User
     end
   end
 end
-
-
-  def recommendation_for user_index
-    top_item_index = nil
-    top_rating = nil
-    aproximated_ratings[user_index].each_with_index do |rating, item_index|
-      if rating && !original_ratings[user_index][item_index]
-        if top_rating
-          if rating > top_rating
-            top_rating = rating
-            top_item_index = item_index
-          end
-        else
-          top_rating = rating
-          top_item_index = item_index
-        end
-      end
-    end
-
-    if top_item_index
-      items[top_item_index]
-    else
-      '?'
-    end
-  end
